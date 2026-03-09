c278bd514d0e48403df0df8d99fe375ce5a46b46
Pranavgami <pranavgami2003@gmail.com>
2026-03-09 23:27:19 +0530

    [ADDED] Base code setup using React + vite for Personal Portfolio.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Contact from "./sections/Contact/Contact";
+import Footer from "./sections/Footer/Footer";
+import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
+import Experience from "./sections/Experience/Experience";
+
+function App() {
+  return (
+    <>
+      <p>Working on Creating Portfollio...</p>
+      <p>Stay tuned...</p>
+    </>
+  );
+}
+
+export default App;
